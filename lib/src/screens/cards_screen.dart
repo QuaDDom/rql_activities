2917c13aba8e389abd0535a05a4fded7750bb276
Jadom <quaddomdev@gmail.com>
2024-07-31 12:39:36 -0300

    [add] basic files

---
+++ lib/src/screens/cards_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class CardsScreen extends StatelessWidget {
+  const CardsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
